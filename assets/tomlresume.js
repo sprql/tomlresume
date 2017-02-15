e148feb9509a71a462b09799da313be7ac67ed11
Alexander Obukhov <github@sprql.space>
2017-02-15 23:56:24 +0300

    Add tomlresume live demo

---
+++ assets/tomlresume.js
@@ -0,0 +1,43 @@
+'use strict';
+
+(function (window) {
+    const tomlTemplateURL = '/templates/resume.toml';
+    const htmlTemplateURL = '/templates/resume.html.mustache';
+
+    let tomlTextarea = document.querySelector('[data-behavior=tomlresume-source]');
+    let templateTextarea = document.querySelector('[data-behavior=tomlresume-template]');
+
+    let tomlTemplateRequest = fetch(tomlTemplateURL).then(function(response) { return response.text(); });
+    let htmlTemplateRequest = fetch(htmlTemplateURL).then(function(response) { return response.text(); })
+
+    let htmlFrame = document.querySelector('[data-behavior=tomlresume-html-resume]');
+
+    let htmlTemplateData = '';
+
+    function render() {
+        let json = toml.parse(tomlTextarea.value);
+        let html = Mustache.render(htmlTemplateData, json);
+
+        htmlFrame.contentDocument.documentElement.innerHTML = html;
+    }
+
+    function print() {
+        htmlFrame.contentWindow.print();
+        return false;
+    }
+
+    Promise.all([tomlTemplateRequest, htmlTemplateRequest]).then(values => {
+        let tomlTemplateData = values[0];
+        htmlTemplateData = values[1];
+
+        tomlTextarea.value = tomlTemplateData;
+
+        render();
+    });
+
+    let printAction = document.querySelector('[data-action=tomlresume-print]');
+    printAction.addEventListener('click', print);
+
+    tomlTextarea.addEventListener('keyup', render);
+    tomlTextarea.addEventListener('change', render);
+})(window);
